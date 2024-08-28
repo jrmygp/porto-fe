f9a35b57f3f38c6fbff209534cabaf43cc79260a
Jeremy <jeremy.puglisi17@gmail.com>
2024-08-28 18:44:56 +0700

    UI : major UI changes in progress

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import AboutMe from "./components/AboutMe/AboutMe";
 import AnimationWrapper from "./components/AnimationWrapper";
 import Footer from "./components/Footer/Footer";
 import ProjectsV2 from "./components/ProjectsV2/ProjectsV2";
+import Background from "./assets/texture1.gif";
 
 function App() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
   const [isVisible, setIsVisible] = useState(true);
   const textRef = useRef(null);
-  const parallaxRef = useRef(null);
 
   const toggleMenu = () => {
     setMenuIsOpen(!menuIsOpen);
@@ -30,7 +30,7 @@ function App() {
 
   useEffect(() => {
     const onMouseMove = (e) => {
-      document.documentElement.style.setProperty("--cursor-x", e.clientX + 5 + "px");
+      document.documentElement.style.setProperty("--cursor-x", e.clientX + "px");
       document.documentElement.style.setProperty("--cursor-y", e.clientY + "px");
     };
 
@@ -83,9 +83,9 @@ function App() {
   // }, [parallaxRef]);
 
   return (
-    <div className="h-full">
-      <div className="hidden lg:block sticky top-0 z-50">
-        <Navbar parallaxRef={parallaxRef} />
+    <div className="flex flex-col gap-20">
+      <div className="hidden lg:block fixed top-0 z-50 w-full">
+        <Navbar />
       </div>
 
       <div className="flex lg:hidden sticky top-10 z-50 justify-end mr-5">
@@ -96,66 +96,36 @@ function App() {
         <MobileMenuList onClick={toggleMenu} />
       </div>
 
-      <Parallax ref={parallaxRef} pages={4} style={{ top: "0", left: "0" }}>
-        <ParallaxLayer offset={0} speed={0}>
-          <section className="h-full" style={{ backgroundImage: "url(/background-full.jpg)", backgroundSize: "cover" }}>
-            <div className={classes.container}>
-              <div className={`${classes["layer"]} ${classes["dark_layer"]}`}>
-                <div
-                  className={`text-6xl lg:text-8xl ${classes["text_container"]}`}
-                  onMouseEnter={onMouseEnter}
-                  onMouseLeave={onMouseLeave}
-                >
-                  <div>JEREMY</div>
-                  <div>GERALD</div>
-                  <div>PUGLISI</div>
-                </div>
-              </div>
-
-              <div className={`${classes["layer"]} ${classes["layer_red"]}`}>
-                <div className={`${`text-6xl lg:text-8xl ${classes["text_container"]}`} ${classes["text_black"]}`}>
-                  <div>SOFTWARE</div>
-                  <div>ENGINEER</div>
-                  <div>DEVELOPER</div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={1}>
-          <div>
-            <AboutMe />
-          </div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2} speed={0.5}>
-          <div id="projects">
-            <div className="rounded-3xl bg-[#e7e8ea] flex flex-col justify-center items-center gap-20 py-20 px-2 lg:px-[400px]">
-              <AnimationWrapper delay={0.15} className="items-center">
-                <h1 className="text-5xl lg:text-8xl text-center text-text-primary font-bold">WHAT I DO</h1>
-              </AnimationWrapper>
-
-              <AnimationWrapper delay={0.5} className="items-center">
-                <div className="flex flex-col items-center gap-1 opacity-55">
-                  <div className={classes.arrow} />
-                  <p className="text-text-primary">SCROLL</p>
-                  <p className="text-text-primary">TO EXPLORE</p>
-                  <div className={classes.arrow} />
-                </div>
-              </AnimationWrapper>
-
-              <AnimationWrapper delay={0.75}>
-                <ProjectsV2 />
-              </AnimationWrapper>
+      <section className="h-screen" style={{ backgroundImage: "url(/background-full.jpg)", backgroundSize: "cover" }}>
+        <div className={classes.container}>
+          <div className={`${classes["layer"]} ${classes["dark_layer"]}`}>
+            <div
+              className={`text-6xl lg:text-8xl ${classes["text_container"]}`}
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+            >
+              <div>JEREMY</div>
+              <div>GERALD</div>
+              <div>PUGLISI</div>
             </div>
           </div>
-        </ParallaxLayer>
 
-        <ParallaxLayer offset={3} speed={1}>
-          <Footer />
-        </ParallaxLayer>
-      </Parallax>
+          <div className={`${classes["layer"]} ${classes["layer_red"]}`}>
+            <div className={`${`text-6xl lg:text-8xl ${classes["text_container"]}`} ${classes["text_black"]}`}>
+              <div>SOFTWARE</div>
+              <div>ENGINEER</div>
+              <div>DEVELOPER</div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="h-screen relative border flex items-center">
+        <div className="absolute top-20 right-20 w-[500px] h-[500px] rounded-full bg-white filter blur-3xl opacity-10 animate-blob" />
+        <AboutMe />
+        {/* <div className="absolute top-10 left-40 w-72 h-72 rounded-full bg-yellow-300 mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+        <div className="absolute top-40 left-20 w-72 h-72 rounded-full bg-pink-300 mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div> */}
+      </section>
 
       <div
         className={`${classes["circle-wrapper"]} transition-opacity duration-500 ${
